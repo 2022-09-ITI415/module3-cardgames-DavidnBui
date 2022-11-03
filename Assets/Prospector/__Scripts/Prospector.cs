9c14ee22fcae9f37aaa3854aaac6782f080385ff
DavidnBui <davidnbui@yahoo.com>
2022-11-03 00:05:38 -0400

    Added a scoring system

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -196,6 +196,7 @@ public class Prospector : MonoBehaviour
 				MoveToDiscard(target); 
 				MoveToTarget(Draw()); 
 				UpdateDrawPile(); 
+				ScoreManager.EVENT(eScoreEvent.draw);
 				break;
 			case eCardState.tableau:
 				bool validMatch = true;
@@ -211,6 +212,7 @@ public class Prospector : MonoBehaviour
 				tableau.Remove(cd);
 				MoveToTarget(cd);
 				SetTableauFaces();
+				ScoreManager.EVENT(eScoreEvent.mine);
 				break;
 		}
 		CheckForGameOver();
@@ -240,11 +242,13 @@ public class Prospector : MonoBehaviour
 	{
 		if (won)
 		{
-			print ("Game Over. You won! :)");
+			//print ("Game Over. You won! :)");
+			ScoreManager.EVENT(eScoreEvent.gameWin);
 		}
 		else
 		{
-			print ("Game Over. You Lost. :(");
+			//print ("Game Over. You Lost. :(");
+			ScoreManager.EVENT(eScoreEvent.gameLoss);
 		}
 		SceneManager.LoadScene("__Prospector_Scene_0");
 	}
