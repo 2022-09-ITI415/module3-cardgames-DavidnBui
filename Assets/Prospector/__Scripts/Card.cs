4ef842265e138e86e77395e3761024503c1e9167
DavidnBui <davidnbui@yahoo.com>
2022-11-02 23:33:18 -0400

    Drawing cards

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -77,6 +77,11 @@ public class Card : MonoBehaviour {
 			}
 		}
 	}
+
+	virtual public void OnMouseUpAsButton()
+    {
+		print(name);
+    }
 	
 	// Update is called once per frame
 	void Update () {
